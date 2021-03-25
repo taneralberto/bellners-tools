a7511d2e3e86b97f5545ff7512c7a75c52b9c92e
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-03-25 16:51:55 -0400

    Had been enhanced file comment.php

--- comments.php
+++ comments.php
@@ -7,7 +7,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package bellner-tools
+ * @package telesur
  */
 
 /*
@@ -19,59 +19,29 @@ if ( post_password_required() ) {
 	return;
 }
 ?>
-
-<div id="comments" class="comments-area">
-
+<div id="comments">
 	<?php
-	// You can start editing here -- including this comment!
-	if ( have_comments() ) :
-		?>
-		<h2 class="comments-title">
-			<?php
-			$bellner_tools_comment_count = get_comments_number();
-			if ( '1' === $bellner_tools_comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'bellner-tools' ),
-					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
-				);
-			} else {
-				printf( 
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $bellner_tools_comment_count, 'comments title', 'bellner-tools' ) ),
-					number_format_i18n( $bellner_tools_comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
-				);
-			}
-			?>
-		</h2><!-- .comments-title -->
-
-		<?php the_comments_navigation(); ?>
-
-		<ol class="comment-list">
-			<?php
-			wp_list_comments(
-				array(
-					'style'      => 'ol',
-					'short_ping' => true,
-				)
-			);
-			?>
-		</ol><!-- .comment-list -->
-
+		if ( have_comments() ) :
+	?>
+<?php the_comments_navigation(); ?>
+	<ol>
 		<?php
-		the_comments_navigation();
-
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() ) :
-			?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'bellner-tools' ); ?></p>
-			<?php
-		endif;
-
-	endif; // Check for have_comments().
-
+			wp_list_comments(
+			array(
+				'style'      => 'ol',
+				'short_ping' => true,
+			)
+		);
+		?>
+	</ol>
+<?php
+the_comments_navigation();
+	if ( ! comments_open() ) :
+?>
+	<p class="no-comments"><?php esc_html_e( 'Los comentarios están cerrados en este post', 'bellner' ); ?></p>
+		<?php
+	endif;
+	endif;
 	comment_form();
 	?>
-
-</div><!-- #comments -->
+</div>
