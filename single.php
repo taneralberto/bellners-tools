dd0b457849e65a1178a96cd195fd82373d33af51
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-03-25 16:04:20 -0400

    Making directories for single

--- single.php
+++ single.php
@@ -16,25 +16,13 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part( 'template-parts/components/templates/single/index');
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'bellner-tools' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'bellner-tools' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
-
-			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif;
 
-		endwhile; // End of the loop.
+		endwhile;
 		?>
-
-	</main><!-- #main -->
-
 <?php
-get_sidebar();
 get_footer();
