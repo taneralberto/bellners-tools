96f3d45530c2f37296d1805477568bbfbafecf0f
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-03-31 17:34:45 -0400

    Was added again filter conflictive, but it fixed into comments.php

--- template-parts/components/molecules/comments/comments.php
+++ template-parts/components/molecules/comments/comments.php
@@ -18,6 +18,40 @@
 if ( post_password_required() ) {
 	return;
 }
+
+add_filter( 'comment_form_defaults', 'bellner_modify_fields_form' );
+
+function bellner_modify_fields_form( $args ){
+
+	$commenter = wp_get_current_commenter();
+	$req = get_option( 'require_name_email' );
+	$aria_req = ( $req ? " aria-required='true'" : '' );
+
+	$author = '<input class="comment-form-comment__input" placeholder="'.__( 'Name' ) . ( $req ? ' *' : '' ).'" id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .'" size="30"' . $aria_req . ' />';
+	$email = '<input class="comment-form-comment__input" placeholder="'.__( 'Email' ) . ( $req ? ' *' : '' ).'" id="email" name="email" type="text" value="' . esc_attr( $commenter['comment_author_email'] ) .'" size="30"' . $aria_req . ' />';
+	$url = '<input class="comment-form-comment__input" placeholder="'.__( 'Website' ).'" id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .'" size="30" /></input>';
+	$comment = '<textarea class="comment-form-comment__textarea" placeholder="'. _x( 'Comment', 'noun' ).'" id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea>';
+
+	$args['fields']['author'] = $author;
+	$args['fields']['email'] = $email;
+	$args['fields']['url'] = $url;
+	$args['comment_field'] = $comment;
+
+	return $args;
+
+}
+
+add_filter( 'comment_form_fields', 'bellner_modify_order_fields' );
+
+function bellner_modify_order_fields( $fields ){
+	$val = $fields['comment'];
+	unset($fields['comment']);
+
+	$fields += array('comment' => $val );
+
+	return $fields;
+}
+
 ?>
 
 <div id="comments" class="comments-area">
