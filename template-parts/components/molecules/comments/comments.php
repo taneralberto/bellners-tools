4300e6cdbf29ba25fd62b299d9ca784019b3a625
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-04-01 12:24:19 -0400

    Finished styles for comments v.2

--- template-parts/components/molecules/comments/comments.php
+++ template-parts/components/molecules/comments/comments.php
@@ -10,25 +10,15 @@
  * @package telesur
  */
 
-/*
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
- */
 if ( post_password_required() ) {
 	return;
 }
-
-
 ?>
-
 <div id="comments" class="comments-area">
-
 	<?php
 	if ( have_comments() ) :
 		?>
 		<?php the_comments_navigation(); ?>
-
 		<ol class="content-area__comments__lista">
 			<?php
 			wp_list_comments(
@@ -39,19 +29,14 @@ if ( post_password_required() ) {
 			);
 			?>
 		</ol>
-
 		<?php
 		the_comments_navigation();
-
 		if ( ! comments_open() ) :
 			?>
 			<p class="no-comments"><?php esc_html_e( 'Los comentarios están cerrados en este post', 'telesur' ); ?></p>
 			<?php
 		endif;
-
 	endif;
-
 	comment_form();
 	?>
-
 </div>
