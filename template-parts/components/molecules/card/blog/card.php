17b820ad0aae059deedfb4d8dfc91c08abc14ef0
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-04-01 16:37:39 -0400

    Added styles for car into slider blog

--- template-parts/components/molecules/card/blog/card.php
+++ template-parts/components/molecules/card/blog/card.php
@@ -2,17 +2,16 @@
     <div class="blog-card__image">
         <?php Atomic::atom( 'thumbnail', ['size' => 'medium'] ); ?>
     </div>
-
+    <div class="blog-card__date">
+        <?php Atomic::atom( 'date-public' ); ?>
+    </div>
     <div class="blog-card__info">
-        <div class="blog-card__title">
+        <p class="blog-card__title">
             <?php Atomic::atom( 'title' ); ?>
-        </div>
+        </p>
 
-        <div class="blog-card__date">
-            <?php Atomic::atom( 'date-public' ); ?>
-        </div>
-        <div class="blog-card__read-more">
+        <p class="blog-card__read-more">
             <?php Atomic::atom( 'read-more' ); ?>
-        </div>
+        </p>
     </div>
 </div>
