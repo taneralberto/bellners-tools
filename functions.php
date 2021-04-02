e50c2843566622f8228e42d839fb2a50bb0cd060
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-04-02 15:35:40 -0400

    Created custom post type: testimonials

--- functions.php
+++ functions.php
@@ -168,6 +168,10 @@ require get_template_directory() . '/inc/template-tags.php';
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/template-functions.php';
+/**
+ * Configure custom post types.
+ */
+require get_template_directory() . '/inc/custom_post_types.php';
 
 /**
  * Customizer additions.
