4787b901a3f231e0f1ed836e690b33059b979a30
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-04-06 11:43:35 -0400

    Created parallax

---
+++ template-parts/components/molecules/parallax/parallax.php
@@ -0,0 +1,5 @@
+<div class="container-parallax">
+	<div class="parallax">
+        <? Atomic::atom('info-parallax'); ?>
+    </div>
+</div>
--- template-parts/components/templates/home/home.php
+++ template-parts/components/templates/home/home.php
@@ -2,6 +2,7 @@
     <?php
         Atomic::organism( 'products-slider' );
         Atomic::organism( 'testimonials' );
+        Atomic::molecule( 'parallax' );
         Atomic::organism( 'block-newsletter' );
         Atomic::organism( 'posts-slider' );
         Atomic::organism( 'our-clients' );
