5450f0eb73708bb69855cef537d2ea5bdb7f9eab
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-04-07 08:35:04 -0400

    Added shortcode for categories list

---
+++ template-parts/components/organisms/list-cat/list-cat.php
@@ -0,0 +1,1 @@
+<?php echo do_shortcode( '[product_categories]' ); ?>
--- template-parts/components/templates/home/home.php
+++ template-parts/components/templates/home/home.php
@@ -1,11 +1,11 @@
 <div class="home">
     <?php
         Atomic::organism( 'products-slider' );
+        Atomic::organism( 'list-cat' );
         Atomic::organism( 'testimonials' );
         Atomic::molecule( 'parallax' );
         Atomic::organism( 'block-newsletter' );
         Atomic::organism( 'posts-slider' );
         Atomic::organism( 'our-clients' );
-        Atomic::organism( 'list-cat' );
     ?>
 </div>
