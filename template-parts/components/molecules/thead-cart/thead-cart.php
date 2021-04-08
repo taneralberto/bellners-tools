91d9d4a0a50dc2ec1ebcd1ebacac9576836ccc30
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-04-08 16:26:04 -0400

    Ufff finally was separated cart.php by atoms, organisms and molecules

---
+++ template-parts/components/molecules/thead-cart/thead-cart.php
@@ -0,0 +1,10 @@
+<thead>
+        <tr>
+            <th class="product-remove">&nbsp;</th>
+            <th class="product-thumbnail">&nbsp;</th>
+            <th class="product-name"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
+            <th class="product-price"><?php esc_html_e( 'Price', 'woocommerce' ); ?></th>
+            <th class="product-quantity"><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></th>
+            <th class="product-subtotal"><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?></th>
+        </tr>
+    </thead>
