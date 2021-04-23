b63f12fdb4b00a4c0e67afa68bf640de8d1067bc
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-04-23 16:20:40 -0400

    Fixed styles for container product

--- template-parts/components/atoms/share-t/share-t.php
+++ template-parts/components/atoms/share-t/share-t.php
@@ -1,3 +1,3 @@
 <a href="https://twitter.com/intent/tweet?text=<?php the_permalink(); ?>" class="twitter" target="_blank">
-<img src="https://i.postimg.cc/tTq3Qjhk/gorjeo.png" alt="Share to twitter">
+<img src="https://i.postimg.cc/Fzt015TL/twitter.png" alt="Share to twitter">
 </a>
--- template-parts/components/atoms/share-w/share-w.php
+++ template-parts/components/atoms/share-w/share-w.php
@@ -1,3 +1,3 @@
 <a href="https://twitter.com/intent/tweet?text=<?php the_permalink(); ?>" class="twitter" target="_blank">
-<img src="https://i.postimg.cc/mg1mqt3R/whatsapp.png" alt="Share to Whatsapp">
+<img src="https://i.postimg.cc/9XTGK0ft/whatsapp.png" alt="Share to Whatsapp">
 </a>
