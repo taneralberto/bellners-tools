e93035e22969b1ac03a51ff0d1c367f60da84aea
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-05-10 17:12:41 -0400

    Finished 404 page

--- template-parts/components/templates/404/404.php
+++ template-parts/components/templates/404/404.php
@@ -1,4 +1,7 @@
 <section class="error-404">
-    <?php Atomic::organism('404-header'); ?>
-
+    <?php
+        Atomic::organism('404-header');
+        Atomic::organism('products-slider');
+        Atomic::organism('posts-slider');
+    ?>
 </section>
