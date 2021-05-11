5090b2534bb49ee0e409f6e275f6bd02b3954a70
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-05-11 11:42:51 -0400

    Finished head about page

---
+++ template-parts/components/molecules/about-us/about-us.php
@@ -0,0 +1,9 @@
+<? Atomic::atom('title'); ?>
+<div class="about-head">
+    <div class="about-head__content">
+        <? Atomic::atom('single-content'); ?>
+    </div>
+    <div class="about-head__img">
+        <? Atomic::atom( 'thumbnail'); ?>
+    </div>
+</div>
---
+++ template-parts/components/templates/about-us/about-us.php
@@ -0,0 +1,2 @@
+<?
+Atomic::molecule('about-us');
