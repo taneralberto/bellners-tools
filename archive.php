be2c324c7c26732606a9abadf542d632b4496de8
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-05-18 11:58:07 -0400

    Added directories for archive product

--- archive.php
+++ archive.php
@@ -20,7 +20,7 @@ get_header();
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
-
+<h1>Hola</h1>
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) :
